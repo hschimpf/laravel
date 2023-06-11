54eff987b6245bab47c838c73bdad7c0e89dc6d6
Hermann D. Schimpf <hschimpf@hds-solutions.net>
2023-06-11 18:23:01 -0400

    Updated Vite config
    * Autoload modules assets
    * Use `APP_URL` as host config
    * Allow to specify an SSL cert and key

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,62 @@
-import { defineConfig } from 'vite';
+import fs from 'fs/promises';
 import laravel from 'laravel-vite-plugin';
+import path from "path";
+import {defineConfig, loadEnv} from 'vite';
 
-export default defineConfig({
-    plugins: [
-        laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
-            refresh: true,
-        }),
-    ],
-});
+export default async ({mode}) => {
+    process.env = {...process.env, ...loadEnv(mode, process.cwd())};
+
+    const host = (process.env.APP_URL ?? 'localhost').replace(/http(s)?:\/\//, '');
+    const https = process.env.VITE_SSL_KEY && process.env.VITE_SSL_CERT ? {
+        key: await fs.readFile(process.env.VITE_SSL_KEY),
+        cert: await fs.readFile(process.env.VITE_SSL_CERT),
+    } : false;
+
+    const assets = [
+        'resources/css/app.scss',
+        'resources/js/app.ts',
+    ];
+    for (const module of await fs.readdir('modules/')) {
+        if (module === '.gitkeep') continue;
+
+        const folders = [
+            `modules/${module}/resources/assets/css`,
+            `modules/${module}/resources/assets/js`,
+        ];
+        for (const folder of folders) {
+            const resources = await fs.readdir(`${folder}/`);
+            resources.forEach(asset => {
+                if (asset.endsWith('.scss') || asset.endsWith('.ts')) {
+                    assets.push(`${folder}/${asset}`);
+                }
+            })
+        }
+    }
+
+    return defineConfig({
+        server: {
+            host,
+            hmr: {host},
+            https: https,
+            watch: {
+                usePolling: true,
+            }
+        },
+        plugins: [
+            laravel({
+                input: assets,
+                refresh: [
+                    'resources/views/**',
+                    'modules/**/resources/views/**',
+                    'modules/**/resources/assets/css/**',
+                    'modules/**/resources/assets/js/**',
+                ],
+            }),
+        ],
+        resolve: {
+            alias: {
+                '~bootstrap': path.resolve(__dirname, 'node_modules/bootstrap'),
+            }
+        },
+    });
+}
