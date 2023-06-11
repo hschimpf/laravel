9989777da5f7582aebfd4d5c65c32570b01b9955
Hermann D. Schimpf <hschimpf@hds-solutions.net>
2023-06-11 18:30:47 -0400

    Configured Pest as unit test

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -1,10 +1,9 @@
-<?php
+<?php declare(strict_types=1);
 
 namespace Tests;
 
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
-abstract class TestCase extends BaseTestCase
-{
+abstract class TestCase extends BaseTestCase {
     use CreatesApplication;
 }
