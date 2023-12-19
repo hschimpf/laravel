b307939a65b1ad8ada3fc484532d06c85f51e2d5
Hermann D. Schimpf <hschimpf@hds-solutions.net>
2023-12-19 11:58:26 -0300

    Updated test expectation message

--- tests/Unit/ModulesTest.php
+++ tests/Unit/ModulesTest.php
@@ -4,7 +4,7 @@ use Nwidart\Modules\Facades\Module;
 
 test('that all modules have at least one test', function() {
     expect($modules = Module::all())
-        ->not()->toBeEmpty('No modules defined');
+        ->not()->toBeEmpty('No modules were defined');
 
     foreach ($modules as $module) {
         expect($module)
