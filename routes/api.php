3950ffb4453d51c6c5fa041a2523fb8816e571b7
Hermann D. Schimpf <hschimpf@hds-solutions.net>
2023-12-19 11:49:29 -0300

    Remove `TODO` tags from stubs to clean up the TODO tasks list

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->group(function () {
-    // TODO:
+Route::middleware('auth:sanctum')->group(static function () {
+    //
 });
