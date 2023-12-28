10cbeb6c97e5f2737dbc908803f883aba2e5041f
Hermann D. Schimpf <hschimpf@hds-solutions.net>
2023-12-28 18:42:34 -0300

    Added Inertia SSR config
    * Also, fixed initial load of Vue component for modules

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,6 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <title inertia>{{ config('app.name', 'Laravel') }}</title>
+        <meta inertia head-key="description" name="description" content="Laravel">
 
         {{-- Fonts --}}
         <link rel="preconnect" href="https://fonts.googleapis.com">
@@ -12,10 +13,13 @@
 
         {{-- Scripts --}}
         @routes
-        @vite([
-            'resources/js/app.ts',
-            "resources/views/{$page['component']}.vue",
-        ])
+        @php
+            $module = explode('::', $page['component']);
+            $component = count($module) > 1
+                ? "modules/{$module[0]}/resources/views/{$module[1]}.vue"
+                : "resources/views/{$page['component']}.vue";
+        @endphp
+        @vite([ 'resources/js/app.ts', 'resources/css/app.css', $component ])
         @inertiaHead
     </head>
     <body class="font-sans antialiased">
