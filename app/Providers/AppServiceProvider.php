31e7a779929586be9af348adda672af6f511679d
Hermann D. Schimpf <hschimpf@hds-solutions.net>
2024-01-23 17:29:31 -0300

    Remove BS5 Paginator config, we are migrating to Tailwind CSS

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,6 @@
 
 namespace App\Providers;
 
-use Illuminate\Pagination\Paginator;
 use Illuminate\Support\ServiceProvider;
 
 final class AppServiceProvider extends ServiceProvider {
@@ -18,7 +17,7 @@ final class AppServiceProvider extends ServiceProvider {
      * Bootstrap any application services.
      */
     public function boot(): void {
-        Paginator::useBootstrapFive();
+        //
     }
 
 }
