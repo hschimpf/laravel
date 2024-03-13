b87c59e0f64ecc6af6698edc28d3981eea1ab065
Hermann D. Schimpf <hschimpf@hds-solutions.net>
2024-03-13 17:06:07 -0300

    FIX: Ziggy routes loading

--- resources/js/app.ts
+++ resources/js/app.ts
@@ -4,7 +4,6 @@ import {createSSRApp, DefineComponent, h} from 'vue';
 import {createInertiaApp} from '@inertiajs/vue3';
 import {resolvePageComponent} from 'laravel-vite-plugin/inertia-helpers';
 import {ZiggyVue} from '@ziggy-js';
-import {Ziggy} from './ziggy.js';
 
 createInertiaApp({
     title: (title: string): string => title.length
@@ -28,7 +27,7 @@ createInertiaApp({
     setup({el, App, props, plugin}): void {
         createSSRApp({render: () => h(App, props)})
             .use(plugin)
-            .use(ZiggyVue, Ziggy)
+            .use(ZiggyVue)
             .mount(el);
     },
 
